ee5fd37b63b01887b5a27059c9b3e619a1c3cfa0
Юлия Павлова <yulya-pligina@yandex.ru>
2024-12-23 16:42:58 +0500

    Add files via upload

--- speech.py
+++ speech.py
@@ -7,17 +7,13 @@ print(translit("""Ladies and gentlemen, I'm 78 years old and I finally got 15 mi
 print(translit("""More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8... \n """, "ru"))
 
 
-from transliterate import get_translit_function
-
-translit_ru = get_translit_function('ru')
-
 print("""78 -""",end=' ')
-print(translit_ru(num2words(78)))
+print(translit((num2words(78)), "ru"))
 print("""15 -""", end=' ')
-print(translit_ru(num2words(15)))
+print(translit((num2words(15)), "ru"))
 print("""3 -""", end=' ')
-print(translit_ru(num2words(3)))
+print(translit((num2words(3)), "ru"))
 print("""40 -""", end=' ')
-print(translit_ru(num2words(40)))
+print(translit((num2words(40)), "ru"))
 print("""8 -""", end=' ')
-print(translit_ru(num2words(8)))
+print(translit((num2words(8)), "ru"))
