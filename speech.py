2d564f764567a139426ba0e1d8d79c2bfb7e971e
Юлия Павлова <yulya-pligina@yandex.ru>
2024-12-23 16:13:53 +0500

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,11 +1,12 @@
 from transliterate import translit
+from num2words import num2words
+
 
 print(translit("""Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen. \n""", "ru"))
 
 print(translit("""More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8... \n """, "ru"))
 
 
-from num2words import num2words
 from transliterate import get_translit_function
 
 translit_ru = get_translit_function('ru')
